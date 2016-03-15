9465b14c295667256fe6e6217065767d39dbb372
GUOLEI <guolei@wofuns.com>
2016-03-15 16:12:23 +0800

    add EZGUI

--- public/games/FightPower/src/ui/game-scene.js
+++ public/games/FightPower/src/ui/game-scene.js
@@ -5,27 +5,37 @@ const BaseLayer = require("./base-layer.js");
 //const image = require("../../assets/ui/img/bomb1.png");
 const Inherited = require("./../inherited");
 const spine = require("pixi-spine");
+const uiJSON = require("../ui-json/game-scene-json");
 
 const GameScene = function (renderer) {
     let that = Inherited(BaseLayer(renderer));
-    var sprite = null;
+    let sprite = null;
     let tween = null;
     let tweenBack = null;
     let position = null;
+
     that.inheritOn("init", function (dt) {
+
+        //EZGUI
+        let guiContainer = null;
+        EZGUI.Theme.load(['./assets/kenney-theme/kenney-theme.json'], function () {
+            guiContainer = EZGUI.create(uiJSON, 'kenney');
+            that.node.addChildAt(guiContainer,0);
+        });
+
         //Tween
         position = {x: 100, y: 100, rotation: 0};
         tween = new EZGUI.Tween(position)
-        .to({x: 600, y: 350, rotation: 180}, 3000)
+            .to({x: 600, y: 350, rotation: 180}, 3000)
             .delay(3000)
             .easing(EZGUI.Easing.Elastic.InOut)
-            ;
+        ;
 
         tweenBack = new EZGUI.Tween(position)
             .to({x: 100, y: 80, rotation: 0}, 3000)
             .delay(3000)
             .easing(EZGUI.Easing.Elastic.InOut)
-            ;
+        ;
         tween.chain(tweenBack);
         tweenBack.chain(tween);
 
@@ -50,10 +60,11 @@ const GameScene = function (renderer) {
                 animation.state.setAnimationByName(0, 'idle', true);
                 that.node.addChild(animation);
             });
-        //EZGUI  unity3D
+
 
 
     });
+
     that.inheritOn("update", function (dt) {
         EZGUI.Tween.update(dt);
         if (sprite) {
