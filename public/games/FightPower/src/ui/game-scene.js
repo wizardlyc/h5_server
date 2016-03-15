7bc67468266a8d446cde21d12685ce340874772d
GUOLEI <guolei@wofuns.com>
2016-03-15 11:07:30 +0800

    fix action

--- public/games/FightPower/src/ui/game-scene.js
+++ public/games/FightPower/src/ui/game-scene.js
@@ -8,14 +8,34 @@ const spine = require("pixi-spine");
 
 const GameScene = function (renderer) {
     let that = Inherited(BaseLayer(renderer));
+    var sprite = null;
+    let tween = null;
+    let tweenBack = null;
+    let position = null;
+    that.inheritOn("init", function (dt) {
+        //Tween
+        position = {x: 100, y: 100, rotation: 0};
+        tween = new EZGUI.Tween(position)
+        .to({x: 600, y: 350, rotation: 180}, 3000)
+            .delay(3000)
+            .easing(EZGUI.Easing.Elastic.InOut)
+            ;
 
-    that.inheritOn("init", function () {
+        tweenBack = new EZGUI.Tween(position)
+            .to({x: 100, y: 80, rotation: 0}, 3000)
+            .delay(3000)
+            .easing(EZGUI.Easing.Elastic.InOut)
+            ;
+        tween.chain(tweenBack);
+        tweenBack.chain(tween);
+
         //sprite
         PIXI.loader
-            .add('bomb1', "./assets/ui/img/bomb1.png")
+            .add('test', "./assets/ui/img/test.png")
             .load(function (loader, resources) {
-                let sprite = new PIXI.Sprite(resources.bomb1.texture);
+                sprite = new PIXI.Sprite(resources.test.texture);
                 that.node.addChild(sprite);
+                tween.start();
             });
         //spine
         PIXI.loader
@@ -34,7 +54,15 @@ const GameScene = function (renderer) {
 
 
     });
+    that.inheritOn("update", function (dt) {
+        EZGUI.Tween.update(dt);
+        if (sprite) {
+            sprite.position.x = position.x;
+            sprite.position.y = position.y;
+            sprite.rotation = position.rotation;
+        }
 
+    });
 
     return that;
 };
